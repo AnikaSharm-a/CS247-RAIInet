60d95d125b653a2a068c34317e1046b6ba7902b7
Anika Sharma <sharmaanika2005@gmail.com>
2025-07-27 15:09:46 -0400

    i got something super basic. needs editing

---
+++ graphicdisplay.h
@@ -0,0 +1,29 @@
+#ifndef GRAPHICDISPLAY_H
+#define GRAPHICDISPLAY_H
+
+#include "view.h"
+#include "window.h"
+#include "cellType.h"
+#include <vector>
+
+class GraphicDisplay : public View {
+    mutable Xwindow xw;
+    int gridSize;
+    int cellWidth;
+    int cellHeight;
+
+    // Keep a snapshot of last drawn characters to allow partial redraw
+    std::vector<std::vector<char>> lastDrawn;
+
+    void drawCell(int r, int c, const class Cell &cell, const class Player *p1) const;
+
+public:
+    GraphicDisplay(int gridSize, int width = 500, int height = 500);
+
+    // Unused in this approach, but left for completeness
+    // void notify(int row, int col, CellType state) override;
+
+    void print(const Game &game, std::ostream &out) const override;
+};
+
+#endif
