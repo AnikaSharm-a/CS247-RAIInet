29db5e431a2e8cf16430335f787db291706c56d4
e32liang <e32liang@uwaterloo.ca>
2025-07-28 16:08:17 -0400

    slightly refactored main for links inserting and ability inserting

--- controller.h
+++ controller.h
@@ -6,6 +6,9 @@
 
 class View;
 class Game;
+class Player;
+class Ability;
+enum class LinkType;
 
 class Controller {
     View* view;
@@ -13,10 +16,17 @@ class Controller {
 
     // Returns false if quit or game over
     bool parseCommand(const std::string &cmd, std::istream &in);
+    static LinkType parseLinkType(const std::string& s);
+    static int parseStrength(const std::string& s);
+    void loadLinksFromFile(const std::string& filename, Player* player, bool isPlayer1);
+    void generateDefaultLinks(Player* player, bool isPlayer1);
 
 public:
     Controller(View* view, Game* game);
     void play(std::istream &in);
+    void setupPlayers(Player* p1, Player* p2, 
+                      const std::string& ability1Str, const std::string& ability2Str,
+                      const std::string& link1File, const std::string& link2File);
 };
 
 #endif
